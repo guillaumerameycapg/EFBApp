2a774745641b992fc447d31e6b37fd73d19b817c
Guillaume Ramey <guillaume.ramey@capgemini.com>
2022-01-10 12:41:31 +0100

    Initial Commit

---
+++ EFBApp/EFBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EFBAppApp.swift
+//  EFBApp
+//
+//  Created by Guillaume Ramey on 10/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct EFBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
